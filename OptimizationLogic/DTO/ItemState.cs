1276e29d58f8e083a76966516865386ec1051ede
Radek Svoboda <radek.svoboda@vsb.cz>
2020-08-11 14:57:43 +0200

    Postion mapping and loader stubs created

---
+++ OptimizationLogic/DTO/ItemState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizationLogic.DTO
+{
+    public enum ItemState : int
+    {
+        Forbidden = -1,
+        Empty = 0,
+        MEB = 1,
+        MQB = 2
+    }
+}
---
+++ OptimizationLogic/DTO/ProductionState.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizationLogic.DTO
+{
+    public class ProductionState
+    {
+        public Queue<ItemState> FutureProductionPlan { get; set; } = new Queue<ItemState>();
+        public Queue<ItemState> ProductionHistory { get; set; } = new Queue<ItemState>();
+        public ItemState[,] WarehouseState { get; set; } = new ItemState[4, 12];
+        public double[,] TimeMatrix { get; set; } = new double[47, 47];
+
+        private Dictionary<PositionCodes, (int row, int col)> _warehousePositionMapping;
+        private Dictionary<PositionCodes, int> _timeMatrixMapping;
+
+        public ProductionState()
+        {
+            _warehousePositionMapping = BuildWarehousePositionMappingDict();
+            _timeMatrixMapping = BuildTimeMatrixPositionMappingDict();
+        }
+
+        private Dictionary<PositionCodes, (int row, int col)> BuildWarehousePositionMappingDict()
+        {
+            return ((PositionCodes[])Enum.GetValues(typeof(PositionCodes))).ToDictionary(code => code, code => {
+                if (code == PositionCodes.Stacker)
+                {
+                    return (2, 0);
+                }
+                else if (code == PositionCodes.Service)
+                {
+                    return (2, 11);
+                }
+                else
+                {
+                    (int numericalPart, char alphabetPart) = ParsePositionCode(code);
+                    int col = (numericalPart - (numericalPart % 2 == 0 ? 0 : 1)) / 2;
+                    int row = alphabetPart == 'A' ? 1 : 0 + (numericalPart % 2 == 0 ? 2 : 0);
+                    return (row, col);
+                }
+            });
+
+            //_warehousePositionMapping[PositionCodes.Stacker] = (2, 0);
+            //_warehousePositionMapping[PositionCodes.Service] = (2, 11);
+
+            //foreach (PositionCodes code in Enum.GetValues(typeof(PositionCodes)))
+            //{
+            //    if (code == PositionCodes.Service || code == PositionCodes.Stacker)
+            //    {
+            //        continue;
+            //    }
+
+            //    (int numericalPart, char alphabetPart) = ParsePositionCode(code);
+
+            //    int row, col;
+            //    if (numericalPart % 2 == 0)
+            //    {
+            //        col = numericalPart / 2;
+            //        row = alphabetPart == 'A' ? 3 : 2;
+            //    }
+            //    else
+            //    {
+            //        col = (numericalPart - 1) / 2;
+            //        row = alphabetPart == 'A' ? 1 : 0;
+            //    }
+            //    _warehousePositionMapping[code] = (row, col);
+            //}
+        }
+
+        private (int numericalPart, char alphabetPart) ParsePositionCode(PositionCodes code) {
+            var pureCode = code.ToString().Skip(1).ToString();
+            var numericalPart = int.Parse(pureCode.Remove(pureCode.Length - 1));
+            var alphabetPart = pureCode[pureCode.Length - 1];
+            return (numericalPart, alphabetPart);
+        }
+
+        private Dictionary<PositionCodes, int> BuildTimeMatrixPositionMappingDict()
+        {
+            return ((PositionCodes[])Enum.GetValues(typeof(PositionCodes))).ToDictionary(code => code, code => {
+                if (code == PositionCodes.Stacker)
+                {
+                    return 0;
+                }
+                else if (code == PositionCodes.Service)
+                {
+                    return TimeMatrix.GetLength(0) - 3;
+                } else
+                {
+                    (int numericalPart, char alphabetPart) = ParsePositionCode(code);
+                    return numericalPart * 2 - (alphabetPart == 'A' ? 1 : 0);
+                }
+            });
+
+
+            //_timeMatrixMapping[PositionCodes.Stacker] = 0;
+            //_timeMatrixMapping[PositionCodes.Service] = TimeMatrix.GetLength(0) - 3;
+
+            //foreach (PositionCodes code in Enum.GetValues(typeof(PositionCodes)))
+            //{
+            //    if (code == PositionCodes.Service || code == PositionCodes.Stacker)
+            //    {
+            //        continue;
+            //    }
+            //    (int numericalPart, char alphabetPart) = ParsePositionCode(code);
+            //    _timeMatrixMapping[code] = numericalPart * 2 - (alphabetPart == 'A' ? 1 : 0);
+            //}
+        }
+
+        public int GetTimeMatrixIndex(PositionCodes code) => _timeMatrixMapping[code];
+        public (int row, int col) GetWarehouseIndex(PositionCodes code) => _warehousePositionMapping[code];
+
+        public void LoadTimeMatrix(string csvPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadWarehouseState(string csvPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadProductionHistory(string csvPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadFutureProductionPlan(string csvPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
