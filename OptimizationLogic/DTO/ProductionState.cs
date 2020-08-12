b26c066cc006c6db118a9e672a8c1b0908d9bbbb
Petr Prokop <prokop.peta@gmail.com>
2020-08-12 10:52:45 +0200

    Add initialization of program using input files

--- OptimizationLogic/DTO/ProductionState.cs
+++ OptimizationLogic/DTO/ProductionState.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,12 @@ namespace OptimizationLogic.DTO
     {
         public Queue<ItemState> FutureProductionPlan { get; set; } = new Queue<ItemState>();
         public Queue<ItemState> ProductionHistory { get; set; } = new Queue<ItemState>();
-        public ItemState[,] WarehouseState { get; set; } = new ItemState[4, 12];
-        public double[,] TimeMatrix { get; set; } = new double[47, 47];
+        public ItemState[,] WarehouseState { get; set; } = new ItemState[WarehouseYDimension, WarehouseXDimension];
+        public double[,] TimeMatrix { get; set; } = new double[TimeMatrixDimension, TimeMatrixDimension];
 
+        private const int TimeMatrixDimension = 47;
+        private const int WarehouseXDimension = 12;
+        private const int WarehouseYDimension = 4;
         private Dictionary<PositionCodes, (int row, int col)> _warehousePositionMapping;
         private Dictionary<PositionCodes, int> _timeMatrixMapping;
 
@@ -71,24 +76,71 @@ namespace OptimizationLogic.DTO
         public int GetTimeMatrixIndex(PositionCodes code) => _timeMatrixMapping[code];
         public (int row, int col) GetWarehouseIndex(PositionCodes code) => _warehousePositionMapping[code];
 
+        private void CheckCorrectInputDimension(string[] arr, int correctLen)
+        {
+            if (arr.Length != correctLen)
+            {
+                throw new Exception("Incorrect input matrix shape");
+            }
+        }
+
         public void LoadTimeMatrix(string csvPath)
         {
-            throw new NotImplementedException();
+            string[] lines = File.ReadAllLines(csvPath);
+            CheckCorrectInputDimension(lines, TimeMatrixDimension);
+            
+            for (int i = 0; i < lines.Length; i++)
+            {
+                String[] row = lines[i].Split(';');
+                CheckCorrectInputDimension(row, TimeMatrixDimension); 
+                for (int j = 0; j < row.Length; j++)
+                {
+                    TimeMatrix[i, j] = double.Parse(row[j], CultureInfo.InvariantCulture);
+                }
+            }
+        }
+
+        private ItemState GetItemState(string str)
+        {
+            switch(str)
+            {
+                case "MQB": 
+                    return ItemState.MQB;
+                case "MEB":
+                    return ItemState.MEB;
+                case "Forbidden":
+                    return ItemState.Forbidden;
+                case "Empty":
+                    return ItemState.Empty;
+                default:
+                    throw new Exception("Wrong item state in warehouse input state.");
+            }
         }
 
         public void LoadWarehouseState(string csvPath)
         {
-            throw new NotImplementedException();
+            string[] lines = File.ReadAllLines(csvPath);
+            CheckCorrectInputDimension(lines, WarehouseYDimension);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                String[] row = lines[i].Split(';');
+                CheckCorrectInputDimension(row, WarehouseXDimension);
+                for (int j = 0; j < row.Length; j++)
+                {
+                    WarehouseState[i, j] = GetItemState(row[j]);
+                }
+            }
         }
 
         public void LoadProductionHistory(string csvPath)
         {
-            throw new NotImplementedException();
+            ProductionHistory = new Queue<ItemState>(File.ReadLines(csvPath).Select(line => GetItemState(line)));
         }
 
         public void LoadFutureProductionPlan(string csvPath)
         {
-            throw new NotImplementedException();
+            FutureProductionPlan = new Queue<ItemState>(File.ReadLines(csvPath).Select(line => GetItemState(line)));
         }
     }
 }
