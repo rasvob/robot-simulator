db3dba07205209e83e443e49e94d852357825f5d
Radek Svoboda <radek.svoboda@vsb.cz>
2020-08-13 09:19:58 +0200

    ViewModels added

---
+++ robot-simulator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using OptimizationLogic;
+using OptimizationLogic.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace robot_simulator.ViewModels
+{
+    public class MainWindowViewModel: BaseViewModel
+    {
+        public NaiveController NaiveController { get; set; }
+        public ProductionState ProductionState { get; set; }
+
+        public ICommand NextStep { get; private set; }
+
+        public MainWindowViewModel(NaiveController naiveController)
+        {
+            NaiveController = naiveController;
+            ProductionState = NaiveController.ProductionState;
+            NextStep = new SimpleCommand((_) => NaiveController.NextStep());
+        }
+    }
+}
