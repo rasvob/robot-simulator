cd1a391e2a3d69e3178b764734e5264a112a37b0
Radek Svoboda <radek.svoboda@vsb.cz>
2020-08-17 11:39:27 +0200

    Queue limit value converter added

---
+++ robot-simulator/ViewModels/ValueConterters/QueueDisplayLimitConverter.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace robot_simulator.ViewModels.ValueConterters
+{
+    class QueueDisplayLimitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var collection = value as IEnumerable<WarehouseItemViewModel>;
+                var limit = int.Parse(parameter as string);
+                return collection.Take(limit).ToList();
+            }
+            catch (Exception exc)
+            {
+                Log.Error(exc.Message);
+            }
+            return new List<WarehouseItemViewModel>();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
