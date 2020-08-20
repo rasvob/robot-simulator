863308b13f02a27ee018a71f167fdce2c2a931f3
Radek Svoboda <radek.svoboda@vsb.cz>
2020-08-20 14:03:45 +0200

    FutureProductionPlanGenerator created

---
+++ OptimizationLogic/StateGenerating/FutureProductionPlanGenerator.cs
@@ -0,0 +1,47 @@
+﻿using OptimizationLogic.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizationLogic.StateGenerating
+{
+    public class FutureProductionPlanGenerator
+    {
+        public double MqbToMebTransitionProbability { get; set; } = 0.5;
+        public double ProbabilityOfStartingInMqbState { get; set; } = 1.0;
+        public int RandomSeed { get; set; } = 13;
+        public Random RandomGenerator { get; set; }
+        public FutureProductionPlanGenerator(double mqbToMebTransitionProbability)
+        {
+            MqbToMebTransitionProbability = mqbToMebTransitionProbability;
+            RandomGenerator = new Random(RandomSeed);
+        }
+
+        public virtual double ComputeFinalMqbToMebTransitionProbability()
+        {
+            return MqbToMebTransitionProbability;
+        }
+
+        public List<ItemState> GenerateSequence(int count)
+        {
+            var res = new List<ItemState>(count);
+
+            var currentState = RandomGenerator.NextDouble() <= ProbabilityOfStartingInMqbState ? ItemState.MQB : ItemState.MEB;
+
+            for (int i = 0; i < count; i++)
+            {
+                res.Add(currentState);
+                currentState = currentState switch
+                {
+                    ItemState.MEB => ItemState.MQB,
+                    ItemState.MQB => RandomGenerator.NextDouble() <= ComputeFinalMqbToMebTransitionProbability() ? ItemState.MEB : ItemState.MQB,
+                    _ => ItemState.MQB
+                };
+            }
+
+            return res;
+        }
+    }
+}
