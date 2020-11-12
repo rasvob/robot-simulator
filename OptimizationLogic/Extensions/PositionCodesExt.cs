6863fdaaa66a80c83fc7069841acc1e0e7f85541
Radek Svoboda <radek.svoboda@vsb.cz>
2020-11-12 12:17:16 +0100

    Added <1, 300>x{A,B,C} posisions code with filtering extension prototype

---
+++ OptimizationLogic/Extensions/PositionCodesExt.cs
@@ -0,0 +1,19 @@
+﻿using OptimizationLogic.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizationLogic.Extensions
+{
+    public static class PositionCodesExt
+    {
+        public static PositionCodes[] FilterPositions(this PositionCodes[] values, int rowsInWarehouse, int columnsInWarehouse)
+        {
+            return null;
+        }
+
+    }
+}
