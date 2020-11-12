059bfa5008d9e03f89ab990f9b7a1ff34ecc529b
Radek Svoboda <radek.svoboda@vsb.cz>
2020-11-12 12:09:28 +0100

    NaiveAsyncController using clean

--- OptimizationLogic/AsyncControllers/NaiveAsyncController.cs
+++ OptimizationLogic/AsyncControllers/NaiveAsyncController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using OptimizationLogic.DTO;
-using OptimizationLogic.Extensions;
+﻿using OptimizationLogic.DTO;
 
 namespace OptimizationLogic.AsyncControllers
 {
